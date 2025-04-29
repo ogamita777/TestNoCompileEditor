11acc38d09505fe7bdf2919f5222c8ad2a719fa8
ogamita777 <ogamita777@gmail.com>
2025-04-29 22:17:12 +0900

    fix

--- Tool/Editor/MakePackage_NoCompileEditor.bat
+++ Tool/Editor/MakePackage_NoCompileEditor.bat
@@ -8,10 +8,9 @@ pushd ..\..
 set PROJECT_ROOT=%CD%
 popd
 
-set UAT_BAT="C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat"
 set PROJECT_PATH=%PROJECT_ROOT%\%PROJECT_NAME%.uproject
 
-%UAT_BAT% ^
+"C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat" ^
 BuildCookRun ^
 -project=%PROJECT_PATH% ^
 -noP4 ^
